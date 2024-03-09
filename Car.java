72ba9d1f769a239cdb7bab164727a9129558cf0e
snehakadam03 <162592755+snehakadam03@users.noreply.github.com>
2024-03-09 12:17:50 -0600

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,73 @@
+//new class
+package com.jspider.CarDekho;
+public class Car {
+	private int id;
+	private String name;
+	private String model;
+	private Double price;
+	private String fuelType;
+	private String brand;
+	private String colour;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getFuelType() {
+		return fuelType;
+	}
+
+	public void setFuelType(String fuelType) {
+		this.fuelType = fuelType;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getColour() {
+		return colour;
+	}
+
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+
+	@Override
+	public String toString() {
+		return "Car [id=" + id + ", name=" + name + ", model=" + model + ", price=" + price + ", fuelType=" + fuelType
+				+ ", brand=" + brand + ", colour=" + colour + "]";
+	}
+}
